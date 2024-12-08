7eae08b1aaf0e4d4b26790993fb05a203313ea32
Arthur <arthrp@users.noreply.github.com>
2024-12-08 21:36:00 +0200

    Add day 2 test and day 1 test improvements

--- src/day1.rs
+++ src/day1.rs
@@ -54,30 +54,23 @@ impl Day1Solver {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const DATA: &str = "3   4\n\
+    4   3\n\
+    2   5\n\
+    1   3\n\
+    3   9\n\
+    3   3\n";
 
     #[test]
     fn solve_works(){
-        let data = "3   4\n\
-        4   3\n\
-        2   5\n\
-        1   3\n\
-        3   9\n\
-        3   3";
-
-        let x = Day1Solver::new(data.to_string());
+        let x = Day1Solver::new(DATA.to_string());
         let res = x.solve();
         assert_eq!(res, 11);
     }
 
     #[test]
     fn solve_additional_works(){
-        let data = "3   4\n\
-        4   3\n\
-        2   5\n\
-        1   3\n\
-        3   9\n\
-        3   3";
-        let d = Day1Solver::new(data.to_string());
+        let d = Day1Solver::new(DATA.to_string());
         let res = d.solve_additional();
         assert_eq!(res, 31);
     }
--- src/day2.rs
+++ src/day2.rs
@@ -12,7 +12,7 @@ impl Day2Solver {
 
         let mut safe_reports = 0;
         for line in lines {
-            if line.len() == 0 { continue; }
+            if line.len() == 0 { continue; } //last line is empty
             let nums: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
             let inc = if self.is_safe(nums) { 1 } else { 0 };
             safe_reports += inc;
@@ -32,5 +32,25 @@ impl Day2Solver {
             }
         }
         true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_works(){
+        let data = "7 6 4 2 1\n\
+        1 2 7 8 9\n\
+        9 7 6 2 1\n\
+        1 3 2 4 5\n\
+        8 6 4 4 1\n\
+        1 3 6 7 9\n\
+        \n\
+        ";
+        let d = Day2Solver::new(data.to_string());
+        let result = d.solve();
+        assert_eq!(result, 2);
     }
 }
