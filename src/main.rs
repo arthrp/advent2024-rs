9135c735d19eb5bb4a02cbded4833e20d4bd2f19
Arthur <arthrp@users.noreply.github.com>
2024-12-11 23:13:10 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{env, fs};
 
-use day2::Day2Solver;
 use day3::Day3Solver;
 
 mod day1;
@@ -10,9 +9,6 @@ mod day3;
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let content = fs::read_to_string(&args[1])?;
-
-    // let solver = Day2Solver::new(content);
-    // let result = solver.solve(true);
 
     let solver = Day3Solver::new(content);
     let result = solver.solve();
